d2e77390fb8bca429b897352496507ead25d0164
Florenciong <florencegutierrezmain@gmail.com>
2023-01-24 13:15:40 +0800

    My First PHP

---
+++ phpdemo/01_Output.php
@@ -0,0 +1,32 @@
+<?php
+
+
+/* echo "Hello World 123456";
+echo '<br>';
+echo 123; */
+
+
+/* echo '<h1>Hello</h1>'; */
+
+//print- similar to echo, but a bit slower
+/* print 'Hello'; */
+
+//print_r - Gives a bit more info can be used to print arrays
+/* print_r('Hello');
+echo '<br>'; */
+/* print_r([1,2,3]); */
+
+
+//var_dump - More info data type and length
+/* var_dump('Hello'); */
+/* var_dump([1,2,3]); */
+
+
+
+
+/* $fname = 'Ryan';  //String can be single or double quotes
+$age = 41; // Integer
+
+echo "$fname is $age years old";
+
+ */
